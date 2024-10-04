d7b3f2b8e6dec944ea9c86f959bc872406f782ff
A-pilip <areoayomide2008@gmail.com>
2024-10-04 13:25:42 +0100

    updating login.js

--- public/script/login.js
+++ public/script/login.js
@@ -2,9 +2,9 @@ var btn = document.getElementById("submit");
 
 btn.addEventListener("click", (event) => {
   var inputedPassword = document.getElementById("password").value;
-  var emailAddress = document.getElementById("emailAddress").value;
+  var username = document.getElementById("username").value;
 
-  emailAddress = emailAddress.trim();
+  username = emailAddress.trim();
 
   if (inputedPassword.length === 0) {
     event.preventDefault();
@@ -22,30 +22,11 @@ btn.addEventListener("click", (event) => {
     }
   }
 
-  if (emailAddress.length === 0) {
+  if (username.length === 0) {
     event.preventDefault();
-    document.getElementById("email-error").style.display = "block";
-    document.getElementById("email-error").innerText =
+    document.getElementById("name-error").style.display = "block";
+    document.getElementById("name-error").innerText =
       "Input you Gmail Address in the field";
-  } else {
-    if (emailAddress.indexOf("@") < 0) {
-      event.preventDefault();
-      document.getElementById("email-error").style.display = "block";
-      document.getElementById(
-        "email-error"
-      ).innerText = `Input you Gmail Address with an @ sign, like ${emailAddress}@gmail.com `;
-    } else {
-      console.log(emailAddress.indexOf("@gmail.com") < 0);
-      if (emailAddress.indexOf("@gmail.com") < 0) {
-        event.preventDefault();
-        document.getElementById("email-error").style.display = "block";
-        document.getElementById(
-          "email-error"
-        ).innerText = `Input a correct Gmail Address, it should have @gmail.com`;
-      } else {
-        document.getElementById("email-error").style.display = "none";
-      }
-    }
   }
 });
 
