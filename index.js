d8a363f84fe7ec29afa1beebbd1435290e1ffb19
A-pilip <areoayomide2008@gmail.com>
2024-10-04 13:04:59 +0100

    adding my index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+import bodyParser from "body-parser";
+import express from "express";
+import env from "dotenv";
+import session from "express-session";
+import { Passport } from "passport";
+import pg from "pg";
+import { Strategy } from "passport-local";
+env.config();
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/message", (req, res) => {
+  res.render("message.ejs");
+});
+
+app.get("/testimonial", (req, res) => {
+  res.render("testimonial.ejs");
+});
+
+app.get("/post-testimonial", (req, res) => {
+  res.render("post-testimonial.ejs");
+});
+
+app.get("/principal-officer", (req, res) => {
+  res.render("principal-officer.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`The app is listenig on port ${port}`);
+});
